0b9fe86a86c57c5441f3238c2741019feaf8622c
Rinko <crimson_rin@yeah.net>
2015-12-13 19:49:24 +0800

    add Feture: print x Hello

--- myGreatProj.c
+++ myGreatProj.c
@@ -4,6 +4,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	printf("Hello world!\n");
+	int n,i;
+	scanf("%d",&n);
+	for(i=0;i<=n-1;i++)
+		printf("Hello world!\n");
 	return 0;
 }
